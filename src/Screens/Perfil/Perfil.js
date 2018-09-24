ae42a445b04a4faf6ab33ede2b4dc3e6cdd42663
Luis Urán <luisuran.bit@gmail.com>
2018-09-24 19:11:02 -0300

    Avance perfil

--- src/Screens/Perfil/Perfil.js
+++ src/Screens/Perfil/Perfil.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Avatar } from 'react-native-elements';
 
 class Perfil extends Component {
   render() {
     return (
-      <View>
-          <Text>MI PERFIL</Text>
+      <View style={styles.fondo}>
+          <Avatar
+            size="xlarge"
+            rounded
+            //source={{uri: "https://www.patchcollection.com/media/catalog/product/cache/1/image/1200x/040ec09b1e35df139433887a97daa66f/s/p/spiderman_extra_large_logo.jpg"}}
+            title="LU"
+          />
+          <Text>Luis Urán</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  fondo: {
+    flex: 1,
+    width: null,
+    height: null,
+    //justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
 
 export default Perfil;
